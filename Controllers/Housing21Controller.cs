28a6214e377720c9353c6ffbe468693b5567617a
raulblideran <raul.blideran@hotmail.com>
2023-09-18 09:45:43 +0100

    Add project files.

---
+++ Controllers/Housing21Controller.cs
@@ -0,0 +1,36 @@
+﻿using Housing21.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+using System.Runtime.ConstrainedExecution;
+
+namespace Housing21.Controllers
+{
+    [Route("api/housing/users")]
+    [ApiController]
+    public class Housing21Controller : ControllerBase
+    {
+        private Housing21appContext _context;
+
+        public Housing21Controller(Housing21appContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User usr)
+        {
+            _context.Users.Add(usr);
+            await _context.SaveChangesAsync();
+            return usr;
+        }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Housing21.Models;
+
+public partial class User
+{
+    
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public DateTime Dob { get; set; }
+
+    public string Phonenumber { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+}
